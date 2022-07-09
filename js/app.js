45977cf2fcb37a5bdfe9147a4f560b45ceccaed9
Your Name <you@example.com>
2022-07-09 10:11:34 -0600

    created 3 different while loops

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let counter = 0;
+while (counter <= 50) {
+    console.log(counter);
+    counter = counter + 1;
+}
+
+let counter2 = 0;
+while (counter2 >= -50){
+    console.log(counter2);
+    counter2 = counter2 -1;
+}
+
+
+let counter3 = 0;
+while (counter3 <= 100) {
+    console.log(counter3);
+    counter3 = counter3 + 2;
+}
