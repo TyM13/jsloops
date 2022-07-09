037856e62d4ccbdf4e7f902f66b83526fde67a50
Your Name <you@example.com>
2022-07-09 13:10:15 -0600

    pratice for loops

--- js/app.js
+++ js/app.js
@@ -1,25 +1,41 @@
-// let counter = 0;
-// while (counter <= 50) {
-//     console.log(counter);
-//     counter = counter + 1;
-// }
+let counter = 0;
+while (counter <= 50) {
+    
+    counter = counter + 1;
+}
 
-// counter = 0;
-// while (counter >= -50){
-//     console.log(counter);
-//     counter = counter -1;
-// }
+for (counter = 0; counter <=50; counter++) {
+    console.log(counter);
+}
 
 
-// counter = 0;
-// while (counter <= 100) {
-//     console.log(counter);
-//     counter = counter + 2;
-// 
+counter = 0;
+while (counter >= -50){
+    
+    counter = counter -1;
+}
+
+for(counter = 0; counter >=-50; counter--){
+    console.log(counter);
+}
+
+
+counter = 0;
+while (counter <= 100) {
+    console.log(counter);
+    counter = counter + 2;
+}
+
+
+for(counter = 0; counter <= 100; counter = counter + 2 ){
+    console.log(counter);
+}
 
 
 let players = [`player1`, `player2`, `player3`];
 let score = [5, 10, 15];
+let injured = [true, false, true];
+
 
 let counter_4 = 0;
     while(counter_4 < players.length){
@@ -27,9 +43,15 @@ console.log (players[counter_4], score [counter_4])
 counter_4 = counter_4 +1;
 }
 
-let injured = [true, false, true];
-if (players = true){
-}else if (players = false){
+
+
+
+
+
+
+
+if (players === true){
+}else if (players === false){
     console.log();
 }else{
     console.log(error)
