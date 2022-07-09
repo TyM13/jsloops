6b2f81beac3cbab2be2ebf7937cb322bd13e3a83
Your Name <you@example.com>
2022-07-09 11:28:37 -0600

    knoweldge check pt2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,36 @@
-let counter = 0;
-while (counter <= 50) {
-    console.log(counter);
-    counter = counter + 1;
+// let counter = 0;
+// while (counter <= 50) {
+//     console.log(counter);
+//     counter = counter + 1;
+// }
+
+// counter = 0;
+// while (counter >= -50){
+//     console.log(counter);
+//     counter = counter -1;
+// }
+
+
+// counter = 0;
+// while (counter <= 100) {
+//     console.log(counter);
+//     counter = counter + 2;
+// 
+
+
+let players = [`player1`, `player2`, `player3`];
+let score = [5, 10, 15];
+
+let counter_4 = 0;
+    while(counter_4 < players.length){
+console.log (players[counter_4], score [counter_4])
+counter_4 = counter_4 +1;
 }
 
-let counter2 = 0;
-while (counter2 >= -50){
-    console.log(counter2);
-    counter2 = counter2 -1;
-}
-
-
-let counter3 = 0;
-while (counter3 <= 100) {
-    console.log(counter3);
-    counter3 = counter3 + 2;
+let injured = [true, false, true];
+if (players = true){
+}else if (players = false){
+    console.log();
+}else{
+    console.log(error)
 }
